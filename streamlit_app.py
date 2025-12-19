d25b02ba0590fcf5a987a65def009e6b759ff5ed
patrik-slechta <patrik.slechta@seznam.cz>
2025-12-19 22:04:17 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,5 +41,6 @@ if ingredients_list:
 
 # new api
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
+
 
